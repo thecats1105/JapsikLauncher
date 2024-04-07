2464a5efd3a58461def856934ab144da4827ffbc
thecats1105 <thecats1105@gmail.com>
2024-04-07 11:03:47 +0900

    🔧Server Host Change & Version Up
    Server Host : http://japsik.kro.kr/nebula/distribution.json -> https://data.japsik.xyz/assets/launcher/distribution.json
    Version : 2.1.1 -> 3.0.1

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://japsik.kro.kr/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://data.japsik.xyz/assets/launcher/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
