14774f70fa242e0ef980f154e244fe6a3b42a281
thecats1105 <thecats1105@gmail.com>
2024-05-22 17:42:59 +0900

    :hammer: Changed assets URL
    Changed to https://static.japsik.xyz/assets/launcher/distribution.json from https://data.japsik.xyz/assets/launcher/distribution.json

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,9 +2,7 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-// Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://data.japsik.xyz/assets/launcher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://static.japsik.xyz/assets/launcher/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
